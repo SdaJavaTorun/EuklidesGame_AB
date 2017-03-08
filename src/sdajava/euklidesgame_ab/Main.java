a0380dfbdc7dc8c37d9ef116f5f6d588100402c4
andrzej667 <abiczyk@gmail.com>
2017-03-08 20:54:20 +0100

    add final files

---
+++ src/sdajava/euklidesgame_ab/Main.java
@@ -0,0 +1,36 @@
+package sdajava.euklidesgame_ab;
+
+import java.util.Random;
+
+public class Main {
+
+
+
+
+
+    public static void main(String[] args) {
+
+        Random token = new Random();
+        int GamerA = token.nextInt(100);
+        int GamerB = token.nextInt(100);
+        int pozostale = 0;
+
+        System.out.println("Gracz A :" + GamerA + " żetonów");
+        System.out.println("Gracz B :" + GamerB + " żetonów");
+
+        while (GamerA !=0 && GamerB != 0) {
+            if (GamerA>GamerB)
+            {
+                GamerA= GamerA - GamerB;
+                System.out.println("Zetony A: " + GamerA);
+            }
+            else {
+                GamerB = GamerB - GamerA;
+                System.out.println("Zetony B: " + GamerB);
+            }
+
+        }
+        pozostale = GamerA + GamerB;
+        System.out.println("Koniec gry " + pozostale);
+    }
+}
