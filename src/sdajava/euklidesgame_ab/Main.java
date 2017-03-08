ef848bd3f935d69cee18aef6de8bd4d4176f9618
andrzej667 <abiczyk@gmail.com>
2017-03-08 21:37:29 +0100

    code format update

--- src/sdajava/euklidesgame_ab/Main.java
+++ src/sdajava/euklidesgame_ab/Main.java
@@ -4,16 +4,13 @@ import java.util.Random;
 
 public class Main {
 
-
-
-
-
     public static void main(String[] args) {
 
         Random token = new Random();
         int GamerA = token.nextInt(100);
         int GamerB = token.nextInt(100);
         int pozostale = 0;
+        pozostale = GamerA + GamerB;
 
         System.out.println("Gracz A :" + GamerA + " żetonów");
         System.out.println("Gracz B :" + GamerB + " żetonów");
@@ -21,16 +18,15 @@ public class Main {
         while (GamerA !=0 && GamerB != 0) {
             if (GamerA>GamerB)
             {
-                GamerA= GamerA - GamerB;
+                GamerA = GamerA - GamerB;
                 System.out.println("Zetony A: " + GamerA);
             }
             else {
                 GamerB = GamerB - GamerA;
                 System.out.println("Zetony B: " + GamerB);
             }
-
         }
-        pozostale = GamerA + GamerB;
-        System.out.println("Koniec gry " + pozostale);
+
+        System.out.println("Koniec gry. " + pozostale);
     }
 }
